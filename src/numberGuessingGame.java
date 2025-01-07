847978e255bbce8b6ea99665246064aaad70f056
krishnalatwall <krishnalatwal7560@gmail.com>
2025-01-07 15:16:43 +0530

    Code for the Number Guessing Game

---
+++ src/numberGuessingGame.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class numberGuessingGame {
+    public static void main(String[] args) {
+        game(); //Function call
+    }
+
+    //Function that implements the number guessing game
+    static void game(){
+        //Scanner class
+        Scanner sc = new Scanner(System.in);
+        int randomNumber = (int) (100 * Math.random()); //Generating a random number b/w 0-100
+
+        //Given 5 chances
+        int chances = 5;
+
+        System.out.println(
+                "A number is chosen"
+                        + " between 1 to 100."
+                        + "Guess the number"
+                        + " within 5 trials.");
+        //Iterate over given chances
+        for (int i = 1; i <= chances ; i++) {
+
+            System.out.print("Guess a number : ");
+            //Take input to guess
+            int guess = sc.nextInt();
+
+            //If the number is guessed
+            if(guess == randomNumber){
+                System.out.println("You guessed it right! \n The number is : " + randomNumber);
+                break;
+            }else if(guess > randomNumber && i != chances){
+                    System.out.println("The number is smaller than : " + guess);
+            } else if (guess < randomNumber && i != chances){
+                System.out.println("The number is higher than : " + guess);
+            }
+
+            if(i == chances){
+                System.out.println("You have exhausted " + chances + " trials.");
+                System.out.println("The number was " + guess);
+            }
+        }
+
+    }
+}
